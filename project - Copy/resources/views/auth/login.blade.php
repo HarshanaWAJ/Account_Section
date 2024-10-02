267c14ad62ab1bebbc14caefd8e88042ef693d41
abskavindi <abskavindi99@gmail.com>
2024-10-02 13:54:42 +0530

    login

---
+++ project - Copy/resources/views/auth/login.blade.php
@@ -0,0 +1,49 @@
+<!-- resources/views/login.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
+
+<body class="page1">
+<header >
+<img src="{{ asset('images\crd logo.png') }}" alt="Logo" class="logo">
+</header>
+
+<main>
+    <section class="login-section">
+        <h2 class="header2">Login</h2>
+        @if ($errors->any())
+            <div class="error">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit" class="login-button">Login</button>
+        </form>
+
+        <p>Don't have an account? <a href="{{ route('register') }}">Register here</a></p>
+    </section>
+</main>
+
+</body>
+
+</html>
