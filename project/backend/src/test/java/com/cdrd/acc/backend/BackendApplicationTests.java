efb25aa63904a1b4405de79add530cfc6f8e252d
HarshanaWAJ <janithharshana2019@gmail.com>
2024-10-02 10:34:07 +0530

    backend

--- project/backend/src/test/java/com/cdrd/acc/backend/BackendApplicationTests.java
+++ project/backend/src/test/java/com/cdrd/acc/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.server;
+package com.cdrd.acc.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
